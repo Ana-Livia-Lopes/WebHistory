8544f07420503a8aeb077cad2c63028ead1e337e
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2024-09-21 13:59:31 -0300

    inicio

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+        <div id="container">
+            <div class="container-side">
+                <div id="menu">
+                    <span class="material-symbols-outlined">
+                        menu
+                    </span>
+                </div>
+                <div class="itens-nav">
+                    <a href="index.html" class="a-sidebar">
+                        <span class="material-symbols-outlined">
+                            home
+                        </span>
+                        <p class="p-sidebar">Início</p>
+                    </a>  
+                </div>
+                <div class="itens-nav">
+                    <a href="financiamento.html" class="a-sidebar">
+                        <span class="material-symbols-outlined">
+                            assignment
+                        </span>
+                        <p class="p-sidebar">Períodos Históricos</p>
+                    </a>
+                </div>
+                <div class="itens-nav">
+                    <a href="educ.html" class="a-sidebar">
+                        <span class="material-symbols-outlined">
+                            school
+                        </span>
+                        <p class="p-sidebar"> Glossário</p>
+                    </a>
+                </div>
+                <div class="itens-nav">
+                    <a href="orcamento.html" class="a-sidebar">
+                        <span class="material-symbols-outlined">
+                            savings
+                        </span>
+                        <p class="p-sidebar">Contato</p>
+                    </a>
+                </div>
+                <div class="itens-nav">
+                    <a href="orcamento.html" class="a-sidebar">
+                        <span class="material-symbols-outlined">
+                            savings
+                        </span>
+                        <p class="p-sidebar">login</p>
+                    </a>
+                </div>
+            </div>
+            <div class="box-img">
+                <a href="index.html" class="a-logo">
+                    <img id="logo-nav" src="img/é" alt="logo">
+                    <p id="invest-p-nav">WebHistory</p>
+                </a> 
+            </div>
+        </div>
+    </header>
+</body>
+</html>
