b3f1a87265e2732f609a7a76d02a7a4b87492ce8
Gabriel-Britoo <gabrito343@gmail.com>
2024-10-03 15:09:16 -0300

    vou criar uma nova sidebar

--- index.php
+++ index.php
@@ -7,62 +7,8 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<header>
-        <div id="container">
-            <div class="container-side">
-                <div id="menu">
-                    <span class="material-symbols-outlined">
-                        menu
-                    </span>
-                </div>
-                <div class="itens-nav">
-                    <a href="index.html" class="a-sidebar">
-                        <span class="material-symbols-outlined">
-                            home
-                        </span>
-                        <p class="p-sidebar">Início</p>
-                    </a>  
-                </div>
-                <div class="itens-nav">
-                    <a href="financiamento.html" class="a-sidebar">
-                        <span class="material-symbols-outlined">
-                            assignment
-                        </span>
-                        <p class="p-sidebar">Períodos Históricos</p>
-                    </a>
-                </div>
-                <div class="itens-nav">
-                    <a href="educ.html" class="a-sidebar">
-                        <span class="material-symbols-outlined">
-                            school
-                        </span>
-                        <p class="p-sidebar"> Glossário</p>
-                    </a>
-                </div>
-                <div class="itens-nav">
-                    <a href="orcamento.html" class="a-sidebar">
-                        <span class="material-symbols-outlined">
-                            savings
-                        </span>
-                        <p class="p-sidebar">Contato</p>
-                    </a>
-                </div>
-                <div class="itens-nav">
-                    <a href="orcamento.html" class="a-sidebar">
-                        <span class="material-symbols-outlined">
-                            savings
-                        </span>
-                        <p class="p-sidebar">login</p>
-                    </a>
-                </div>
-            </div>
-            <div class="box-img">
-                <a href="index.html" class="a-logo">
-                    <img id="logo-nav" src="img/é" alt="logo">
-                    <p id="invest-p-nav">WebHistory</p>
-                </a> 
-            </div>
-        </div>
+    <header>
+        <!-- vou criar uma side navbar do zero -->
     </header>
 </body>
 </html>
