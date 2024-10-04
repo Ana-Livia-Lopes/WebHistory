51dbee2a018c9093b47bd990d2f1c51865472376
Gabriel-Britoo <gabrito343@gmail.com>
2024-10-03 22:57:21 -0300

    começando a sidebar

---
+++ main.js
@@ -0,0 +1,10 @@
+// Código sidebar
+
+let butMenu = document.querySelector('#but-menu');
+let sidebar = document.querySelector('.sidebar');
+
+butMenu.onclick = function() {
+    sidebar.classList.toggle('active');
+}
+
+// Fim código sidebar
