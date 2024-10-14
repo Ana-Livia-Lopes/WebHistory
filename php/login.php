b2eb95c575bdd8da886d47797cdea0423ab0bee6
Gabriel-Britoo <gabrito343@gmail.com>
2024-10-14 16:40:52 -0300

    Criação da pasta "php"; Começo do login e cadastro;

---
+++ php/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/login.css">
+    <title>Login - História na Web</title>
+</head>
+<body>
+    <div>
+        <form action="" method="POST">
+            <label for="email">Email</label>
+            <input type="email" name="email">
+            <label for="senha">Senha</label>
+            <input type="password" name="senha">
+            <select name="" id="">
+                <option value="user-comum">Usuário comum</option>
+                <option value="admin">Administrador</option>
+            </select>
+            <p>Não tem uma conta? <a href="./php/cadastro.php">Cadastre-se!</a></p>
+            <p><a href="">Voltar para o início</a></p>
+        </form>
+    </div>
+</body>
+</html>
