1ac807bc6acbc93c0a0bc0c7f3abcbfd8fe9dac3
Isadora-Gomes <isasilvacpv@gmail.com>
2024-10-17 16:11:06 -0300

    glossario

---
+++ php/glossario.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Glossário</title>
+</head>
+<body>
+    <section>
+    <div>
+        <h2>palavra</h2>
+        <p></p>
+        <p></p>
+        <img src="" alt="">
+    </div>
+
+    <div>
+        <h2>palavra</h2>
+        <p></p>
+        <p></p>
+        <img src="" alt="">
+    </div>
+    </section>
+
+</body>
+</html>
