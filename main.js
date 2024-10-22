06deeff06e2333d948e8f8c251d0a9bfeb1b5d7e
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2024-10-22 08:50:35 -0300

    1

--- main.js
+++ main.js
@@ -8,4 +8,31 @@ butMenu.onclick = function() {
 }
 // Fim código sidebar
 AOS.init();
+
+function banner1() {
+    section = document.querySelector('#historia-primitiva')
+    section.scrollIntoView({ behavior: 'smooth' });
+    return;
+}
+function banner2() {
+    section = document.querySelector('#historia-antiga')
+    section.scrollIntoView({ behavior: 'smooth' });
+    return;
+}
+function banner3() {
+    section = document.querySelector('#idade-media')
+    section.scrollIntoView({ behavior: 'smooth' });
+    return;
+}
+function banner4() {
+    section = document.querySelector('#idade-moderna')
+    section.scrollIntoView({ behavior: 'smooth' });
+    return;
+}
+function banner5() {
+    section = document.querySelector('#idade-comtemporanea')
+    section.scrollIntoView({ behavior: 'smooth' });
+    return;
+}
+
 // fim codgo index
