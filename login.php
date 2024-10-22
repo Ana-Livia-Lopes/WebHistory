27288d8226f7380530f054626abae1a3e00ea5e7
Gabriel-Britoo <gabrito343@gmail.com>
2024-10-22 08:38:50 -0300

    termino php e html login

--- login.php
+++ login.php
@@ -17,6 +17,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION['tipo'] = $usuario_logado['tipo_usuario'];
         
         header('Location: auxiliar.php');
+    } else {
+        echo "<p style='color:red;'>Email ou senha incorretos</p>";
     }
 }
 ?>
